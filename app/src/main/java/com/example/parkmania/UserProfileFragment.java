ea5834755471107d3d0671604447a5619bfb59fd
dhiraj97 <dhirajramnani86@gmail.com>
2021-06-15 10:27:29 -0400

    Updating Email!!

--- app/src/main/java/com/example/parkmania/UserProfileFragment.java
+++ app/src/main/java/com/example/parkmania/UserProfileFragment.java
@@ -1,12 +1,29 @@
 package com.example.parkmania;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.parkmania.session_manager.Session;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +40,9 @@ public class UserProfileFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private EditText email;
+    private Button btnUpdateEmail;
+    private String userId = "Temp_Id";
 
     public UserProfileFragment() {
         // Required empty public constructor
@@ -59,6 +79,50 @@ public class UserProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_user_profile, container, false);
+
+        Session session = new Session(getActivity());
+        if (session.isIsLogin()) {
+            userId = session.getUserId();
+        }
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        email = view.findViewById(R.id.edtEmail);
+        email.setText(user.getEmail());
+
+        btnUpdateEmail = view.findViewById(R.id.btnEmailUpdate);
+        btnUpdateEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                // Get auth credentials from the user for re-authentication
+                AuthCredential credential = EmailAuthProvider
+                        .getCredential(user.getEmail(), "djrocks"); // Current Login Credentials \\
+                // Prompt the user to re-provide their sign-in credentials
+                user.reauthenticate(credential)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Log.d(TAG, "User re-authenticated.");
+                                //Now change your email address \\
+                                //----------------Code for Changing Email Address----------\\
+                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                user.updateEmail(email.getText().toString())
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Log.d(TAG, "User email address updated.");
+                                                }
+                                            }
+                                        });
+                                //----------------------------------------------------------\\
+                            }
+                        });
+            }
+        });
+        return view;
     }
 }
